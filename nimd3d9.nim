2f563aa782dc405b505801ea1ebdda8adb8dadbf
Schelz <67759165+SadSchelz@users.noreply.github.com>
2023-05-29 03:09:39 +0300

    Update nimd3d9.nim

--- nimd3d9.nim
+++ nimd3d9.nim
@@ -5,18 +5,18 @@ import winim/lean
 {.emit:"""
 #include "Include/d3d9.h"
 
-LPDIRECT3D9 d3d;    // the pointer to our Direct3D interface
-LPDIRECT3DDEVICE9 d3ddev;    // the pointer to the device class
+LPDIRECT3D9 d3d; 
+LPDIRECT3DDEVICE9 d3ddev;
 
 void initD3D(HWND hWnd)
 {
-    d3d = Direct3DCreate9(D3D_SDK_VERSION);    // create the Direct3D interface
-    D3DPRESENT_PARAMETERS d3dpp;    // create a struct to hold various device information
+    d3d = Direct3DCreate9(D3D_SDK_VERSION);  
+    D3DPRESENT_PARAMETERS d3dpp;
 
-    ZeroMemory(&d3dpp, sizeof(d3dpp));    // clear out the struct for use
-    d3dpp.Windowed = TRUE;    // program windowed, not fullscreen
-    d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;    // discard old frames
-    d3dpp.hDeviceWindow = hWnd;    // set the window to be used by Direct3D
+    ZeroMemory(&d3dpp, sizeof(d3dpp)); 
+    d3dpp.Windowed = TRUE; 
+    d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;  
+    d3dpp.hDeviceWindow = hWnd;  
     d3d->CreateDevice(D3DADAPTER_DEFAULT,
         D3DDEVTYPE_HAL,
         hWnd,
@@ -28,19 +28,15 @@ void render_frame(void)
 {
     // clear the window to a deep blue
     d3ddev->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 40, 100), 1.0f, 0);
-
-    d3ddev->BeginScene();    // begins the 3D scene
-
-    // do 3D rendering on the back buffer here
-
-    d3ddev->EndScene();    // ends the 3D scene
-
-    d3ddev->Present(NULL, NULL, NULL, NULL);   // displays the created frame on the screen
+    d3ddev->BeginScene(); 
+    
+    d3ddev->EndScene(); 
+    d3ddev->Present(NULL, NULL, NULL, NULL);
 }
 void cleanD3D(void)
 {
-    d3ddev->Release();    // close and release the 3D device
-    d3d->Release();    // close and release Direct3D
+    d3ddev->Release();   
+    d3d->Release();  
 }
 
 """.}
